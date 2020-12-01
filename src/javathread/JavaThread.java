74b932eb8ebcfe4a8717555e5e7141e3b12d7d00
sesana.federico <sesana.federico@TAVE-PC05.OSDBSESTO.local>
2020-12-01 13:31:09 +0100

    frufru

--- src/javathread/JavaThread.java
+++ src/javathread/JavaThread.java
@@ -14,11 +14,31 @@ public class JavaThread {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Process T1 = new Process("T1");
-        Process T2 = new Process("T2");
-        T1.start();
-        T2.start();
+    public static void main(String[] args) throws InterruptedException {
+//        Process T1 = new Process("T1");
+//        Process T2 = new Process("T2");
+//        T1.start();
+//        T2.start();
+//        Thread.sleep(5000);
+//        T1.stop();
+
+//        RunnableProcess p1 = new RunnableProcess("pp1");
+//        Thread t1 = new Thread(p1);
+//        t1.start();
+//        
+//        Thread.sleep(5000);
+//        p1.stup();
+
+        BankAccount account = new BankAccount(2000);
+        Atm atm1 = new Atm(account);
+        Atm atm2 = new Atm(account);
+        Thread juan = new Thread(atm1);
+        Thread mawiateresa = new Thread(atm2);
+        
+        juan.start();
+        mawiateresa.start();
+        atm1.add(500);
+        atm2.add(800);  
     }
     
 }
